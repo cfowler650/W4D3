11cb2f5e58abc0bf77d874efd9206c72d54807da
cfowler650 <calebgithub@gmail.com>
2019-06-05 15:05:40 -0700

    Finish User model

--- 99catspart2/app/models/user.rb
+++ 99catspart2/app/models/user.rb
@@ -15,5 +15,39 @@ class User < ApplicationRecord
   validates :password_digest, presence: true
   validates :password, length:  { minimum: 6 }
 
+  attr_reader :password
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end 
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw) 
+  end
+
+  after_initialize :ensure_session_token
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end 
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
   
+    return nil unless user 
+    
+    user.is_password?(password) ? user : nil 
+  end
+
+
+
+
+
 end
