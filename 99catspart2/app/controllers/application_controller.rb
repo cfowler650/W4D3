43e3d8d9568403ed78eda7cc311bcf0dc7af1a71
cfowler650 <calebgithub@gmail.com>
2019-06-05 18:09:20 -0700

    Fix Sign Up Page

--- 99catspart2/app/controllers/application_controller.rb
+++ 99catspart2/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  # skip_before_action :verify_authenticity_token
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(session_token: Session[:session_token])
+  end
 end
--- 99catspart2/app/controllers/sessions_controller.rb
+++ 99catspart2/app/controllers/sessions_controller.rb
@@ -2,15 +2,39 @@ class SessionsController < ApplicationController
 
   def new
     @user = User.new
-    render :new 
+    render :new
   end
 
   def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+      )
+
+    @user.reset_session_token! unless nil
+     
+    redirect_to cats_url
     
   end 
 
+  def log_in!
+    @current_user.reset_session_token! if @current_user = nil
+    session[:session_token] = @current_user.session_token 
+    redirect_to cats_url
+   
+  end
+
+  def log_out!
+    @current_user.reset_session_token! if current_user
+    session[:session_token] = nil
+    @current_user = nil
+  end  
+
   def destroy
-    
+    @current_user = current_user
+    @current_user.delete!
+    log_out!
+    redirect_to new_session_url
   end 
 
 end
